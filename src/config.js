a2fca0823184f571c16638dc9f0b03398f688cdd
Jim Pick <jim@backblend.com>
2018-11-30 04:54:40 -0800

    Change rendezvous server and app name

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   peerStar: {
     ipfs: {
       // swarm: []
-      swarm: ['/dns4/ws-star1.par.dwebops.pub/tcp/443/wss/p2p-websocket-star'],
+      swarm: ['/dns4/rendezvous.jimpick.com/tcp/9091/wss/p2p-websocket-star'],
       // swarm: ['/ip4/127.0.0.1/tcp/9090/ws/p2p-websocket-star']
       // swarm: ['/dns4/ws-star2.sjc.dwebops.pub/tcp/443/wss/p2p-websocket-star'],
       bootstrap: [
