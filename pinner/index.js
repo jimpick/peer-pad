fc2e9d5b94053cad470b5a143f6cd1870614586f
Jim Pick <jim@backblend.com>
2019-02-27 19:44:21 -0800

    Save/load pinner state from file
    Even if ~/.jsipfs/datastore is deleted, the last saved
    state will be restored. Currently doesn't handle sub-collaborations.

--- pinner/index.js
+++ pinner/index.js
@@ -1,15 +1,16 @@
 /* eslint no-console: "off" */
 'use strict'
 
-const debug = require('debug')('peer-base:pinner')
+const fs = require('fs')
+const debug = require('debug')('peer-pad:pinner')
 const EventEmitter = require('events')
 const Collaboration = require('peer-base/src/collaboration')
 const IPFS = require('peer-base/src/transport/ipfs')
 const PeerCountGuess = require('peer-base/src/peer-count-guess')
-const { decode } = require('delta-crdts-msgpack-codec')
+const { decode, encode } = require('delta-crdts-msgpack-codec')
 
 const defaultOptions = {
-  collaborationInnactivityTimeoutMS: 60000
+  collaborationInactivityTimeoutMS: 10000
 }
 
 class AppPinner extends EventEmitter {
@@ -89,7 +90,7 @@ class AppPinner extends EventEmitter {
   }
 
   async _onGossipMessage (message) {
-    debug('gossip message from %s', message.from)
+    // debug('gossip message from %s', message.from)
     this.emit('gossip', message)
     const peerInfo = await this.ipfs.id()
     if (message.from === peerInfo.id) {
@@ -112,6 +113,20 @@ class AppPinner extends EventEmitter {
         collaboration = this._addCollaboration(collaborationName, type)
         await collaboration.start()
         this.emit('collaboration started', collaboration)
+        try {
+          const backup = fs.readFileSync('./backup.txt', 'utf8')
+          for (let line of backup.split('\n')) {
+            if (line.startsWith(collaborationName + ' ')) {
+              const encodedDelta = line.slice(collaborationName.length + 1)
+              const delta = decode(Buffer.from(encodedDelta, 'base64'))
+              console.log(delta)
+              collaboration.shared.apply(delta)
+            }
+          }
+        } catch (e) {
+          console.error('Exception:', e)
+        }
+        console.log('Jim new collab pinner state as delta', collaboration.shared.stateAsDelta())
       }
     }
     collaboration.deliverRemoteMembership(membership).catch((err) => {
@@ -128,7 +143,7 @@ class AppPinner extends EventEmitter {
     const collaboration = Collaboration(true, this.ipfs, this._globalConnectionManager, this, name, type, options)
     this._collaborations.set(name, collaboration)
 
-    const onInnactivityTimeout = () => {
+    const onInactivityTimeout = () => {
       debug('collaboration %j timed out. Removing it...', name, type)
       collaboration.removeListener('state changed', onStateChanged)
       this._collaborations.delete(name)
@@ -148,11 +163,25 @@ class AppPinner extends EventEmitter {
       if (activityTimeout) {
         clearTimeout(activityTimeout)
       }
-      activityTimeout = setTimeout(onInnactivityTimeout, this._options.collaborationInnactivityTimeoutMS)
+      activityTimeout = setTimeout(onInactivityTimeout, this._options.collaborationInactivityTimeoutMS)
     }
 
     const onStateChanged = () => {
       debug('state changed in collaboration %s', name)
+
+      const fqn = collaboration.fqn()
+      let backup = ''
+      const delta = collaboration.shared.stateAsDelta()
+      console.log('Jim changed pinner state as delta', delta)
+      backup += `${fqn} ${encode(delta).toString('base64')}\n`
+      for (let name of collaboration._subs.keys()) {
+        const sub = collaboration._subs.get(name)
+        const subDelta = sub.shared.stateAsDelta()
+        console.log(` Sub ${name}:`, subDelta)
+        backup += `${fqn}:${name} ${encode(subDelta).toString('base64')}\n`
+      }
+      fs.writeFileSync('./backup.txt', backup)
+
       resetActivityTimeout()
     }
 
